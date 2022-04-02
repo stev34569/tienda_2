7399997b5ced753855d30625633f66eac9fe41b0
Jarod50698 <98364069+Jarod50698@users.noreply.github.com>
2022-04-02 16:36:01 -0600

    Semana10

--- src/main/java/com/tienda/WebConfig.java
+++ src/main/java/com/tienda/WebConfig.java
@@ -4,27 +4,37 @@ import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
-    
+public class WebConfig implements WebMvcConfigurer {
+
     @Bean
-    public SessionLocaleResolver localeResolver(){
-    var slr= new SessionLocaleResolver();
-    slr.setDefaultLocale(new Locale("es"));
-    return slr;
+    public SessionLocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("es"));
+        return slr;
     }
+
     @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor(){
-    var lci= new LocaleChangeInterceptor();
-    lci.setParamName("lang");
-    return lci;
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
-    
-    public void addIntercepters(InterceptorRegistry registro){
-    registro.addInterceptor(localeChangeInterceptor());
+
+    public void addIntercepters(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
     }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro) {
+        registro.addViewController("/").setViewName("index");
+        registro.addViewController("/login");
+        registro.addViewController("/errores/403").setViewName("/errores/403");
+    }
+
 }
